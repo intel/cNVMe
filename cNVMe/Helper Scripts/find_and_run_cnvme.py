6ea91666802db03c1fe7e2644ee2e5a4373f9ef0
csm10495 <csm10495@gmail.com>
2017-06-24 21:41:41 -0700

    better the Windows testing

---
+++ cNVMe/Helper Scripts/find_and_run_cnvme.py
@@ -0,0 +1,29 @@
+'''
+Brief:
+    Quick script to find / run cNVMe
+        Returns the success/failure of main()
+        
+Author:
+    Charles Machalow
+'''
+import os
+import sys
+
+def getExeExtension():
+    if os.name == 'nt':
+        return 'exe'
+    else:
+        return 'out'
+        
+if __name__ == '__main__':
+    cnvmeTopDir = os.path.abspath(os.path.join(os.path.abspath(__file__), os.pardir, os.pardir, os.pardir)) # up 3 dirs brings us to cNVMe\
+    for root, dirs, files in os.walk(cnvmeTopDir):
+        for file in files:
+            filePath = os.path.join(root, file)
+            if file == ('cNVMe.%s' % getExeExtension()):
+                print 'Found cNVMe: %s' % filePath
+                sys.exit(os.system(filePath))
+                
+    print 'Couldn\'t find cNVMe'
+    sys.exit(-1) # faillure to find exe
+                
