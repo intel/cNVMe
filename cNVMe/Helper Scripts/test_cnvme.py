b2c24378143d2e454b41af2e26f853569a993836
csm10495 <csm10495@gmail.com>
2018-01-25 18:55:45 -0800

    appveyor fixes

--- cNVMe/Helper Scripts/test_cnvme.py
+++ cNVMe/Helper Scripts/test_cnvme.py
@@ -11,14 +11,25 @@ import pytest
 from ctypes import CDLL
 from find_and_run_cnvme import findAndRunCnvme, getCnvmePath, getDllExtension
 
+def getOutputType():
+    if os.name != 'nt': # travis
+        return os.environ['OUTPUT_TYPE']
+
+    # appveyor
+    config = os.environ['CONFIGURATION']
+    if 'DLL' in config:
+        return 'DLL'
+
+    return 'EXE'
+
 def test_exe():
-    if os.environ['OUTPUT_TYPE'] != 'EXE':
+    if getOutputType() != 'EXE':
         pytest.skip("Skipping test since this is a dll build")
         
     assert findAndRunCnvme() == 0
 
 def test_dll():
-    if os.environ['OUTPUT_TYPE'] != 'DLL':
+    if getOutputType() != 'DLL':
         pytest.skip("Skipping test since this is an exe build")
         
     cnvme = getCnvmePath(getDllExtension())
