73d6caa336abadafaf2fb9196587b50a305c6812
csm10495 <csm10495@gmail.com>
2018-01-25 19:42:49 -0800

    more updates and fixes for matrix testing

---
+++ cNVMe/Helper Scripts/test_runner.bat
@@ -0,0 +1,20 @@
+if "%PLATFORM%" == "x64" GOTO x64
+
+echo 32bit Platform
+C:\Python27\python -m pip install pytest
+C:\Python27\python -m pytest -s "%~dp0/test_cnvme.py" || GOTO FAIL
+
+GOTO END
+
+:X64
+
+echo 64bit Platform
+C:\Python27-x64\python -m pip install pytest
+C:\Python27-x64\python -m pytest -s "%~dp0/test_cnvme.py" || GOTO FAIL
+
+GOTO END
+
+FAIL:
+exit /b 1
+
+:END
